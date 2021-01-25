511fc5946455a442543cbd50317adf1ea8619a16
Nilüfer Polat <nilufer.polat@bulutmd.com>
2021-01-25 16:31:33 +0300

    added backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.end("Backend");
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit("welcome");
+    socket.on("disconnect", () => console.log("user disconnected"))
+
+    socket.on('current-color', (color) => {
+        console.log(color);
+        socket.broadcast.emit('receive-color', color);
+    })
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
